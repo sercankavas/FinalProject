1d3b505ff0c8fdc971cf4429a922e77a703714f8
Sercan <srsr141986@gmail.com>
2021-03-08 23:10:47 +0100

    .

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -8,5 +8,12 @@ namespace DataAccess.Concrete.EntityFramework
     //Context: DB tablolari ile proje claslarini baglamaya yarar
     public class NorthwindContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //normalde parantezin icine url server yazilir
+            //serverin adini yaziyoruz parantezin icine
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        //dk 1.39
     }
 }
