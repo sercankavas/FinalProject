94691a6dd878391a90ba40b9a9c82649c1681f02
Sercan <srsr141986@gmail.com>
2021-03-08 22:39:55 +0100

    .

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -7,7 +7,10 @@ using System.Text;
 namespace DataAccess.Abstract
 {
     //generic constraint yapacam where ile
-    public interface IEntityRepository<T> where T:class,IEntity
+    //IEntity veya IEntity implemente eden bir nesne olabilir(Category,Customer ve Product nesneleri)
+    //new() : new lenebilir olmali, yani interface olmaz(IEntity olmaz),onun implemente ettikleri olabilir
+    //bu haliyle database verileriyle calisan bir interfaceimiz oldu
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
         T Get(Expression<Func<T, bool>> filter);
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: DB tablolari ile proje claslarini baglamaya yarar
+    public class NorthwindContext:DbContext
+    {
+    }
+}
