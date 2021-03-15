b36e22c47d8d1d2287c0246125dd523fbb6c9eb9
Sercan <srsr141986@gmail.com>
2021-03-15 14:46:20 +0100

    15/03

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,8 +12,8 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ProductTest();
-            CategoryTest();
+            ProductTest();
+            //CategoryTest();
         }
 
         private static void CategoryTest()
@@ -29,9 +29,9 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+" / "+ product.CategoryName);
 
             }
         }
