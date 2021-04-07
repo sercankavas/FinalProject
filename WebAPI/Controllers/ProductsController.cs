a9d4418a42b30fc1cc428d9d4dd553b8833d4464
Sercan <srsr141986@gmail.com>
2021-04-07 09:34:24 +0200

    07.04 API setup

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "hei";
+        }
+    }
+}
